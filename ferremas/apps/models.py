86ea15b6f54137a6afb4151afc2daf9fdbc9e1e6
Trinidad <mar.gaetem@duocuc.cl>
2024-06-11 16:03:41 -0400

    pytest

--- ferremas/apps/models.py
+++ ferremas/apps/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
 class Herramienta(models.Model):
     nombre = models.CharField(max_length=100)
     marca = models.CharField(max_length=50)
@@ -10,9 +11,7 @@ class Herramienta(models.Model):
     def __str__(self):
         return f"{self.nombre} - {self.precio}"
     
-
-
-
+    
 class Carrito(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     herramienta = models.ForeignKey(Herramienta, on_delete=models.CASCADE)
